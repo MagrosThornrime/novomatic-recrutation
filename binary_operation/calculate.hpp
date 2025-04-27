2c75cb21e080edda18837da4e4ceaa744d94a237
MagrosThornrime <magros.thornrime@gmail.com>
2025-04-27 07:41:28 +0200

    Start implementing calculate()

---
+++ binary_operation/calculate.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <concepts>
+
+template <class T, class Op>
+concept IsSupported = requires(T first, T second, Op operation)
+{
+    { operation(first, second) } -> std::convertible_to<T>;
+    { T::identity() } -> std::convertible_to<T>;
+};
+
+template <class BinaryOp, class ValueType>
+requires IsSupported<ValueType, BinaryOp>
+ValueType calculate(int n, ValueType value, const BinaryOp& f){
+    ValueType result = ValueType::identity();
+    for(int i = 0; i < n; i++){
+        result = f(result, value);
+    }
+    return result;
+}
---
+++ binary_operation/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "calculate.hpp"
+#include <string>
+#include <sstream>
+
+struct Value
+{
+    std::string value;
+
+    static Value identity()
+    {
+        return Value("");
+    }
+
+    template<typename T>
+    operator T() const
+    {
+        std::stringstream ss(value);
+        T convertedValue;
+        if ( ss >> convertedValue ) return convertedValue;
+        throw std::runtime_error("conversion failed");
+    }
+};
+
+struct Concatenate
+{
+    Value operator()(Value& left, Value& right) const
+    {
+        return Value(left.value + right.value);
+    }
+};
+
+int main() {
+    Value result = calculate<Concatenate, Value>(3, Value("a"), Concatenate());
+    std::cout << result.value << std::endl;
+}
